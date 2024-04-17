d588ecac858b5b1a23e13abea965b7aa4d826b84
Valentin Altana <ambient_soiree.0f@icloud.com>
2024-04-17 17:02:54 +0200

    feat: 2 hour countdown

--- scripts/2-hour-program.js
+++ scripts/2-hour-program.js
@@ -1,10 +1,7 @@
 function twoHourProgramDisplayed() {
-    // TIMER
     const timerDisplayed = document.querySelector(".selected-program")
-    // COUNTDOWN
     const countdownDisplayed = document.querySelector(".countdown")
-
-    if (timerDisplayed.innerText !== "Tu as sélectionné deux heures. Prêt.e ? Pour démarrer la session, appuie sur START." && countdownDisplayed.innerText !== "02:00:00") {
+    if (countdownDisplayed.innerText !== "02:00:00") {
         timerDisplayed.innerText = "Tu as sélectionné deux heures. Prêt.e ? Pour démarrer la session, appuie sur START."
         countdownDisplayed.innerText = "02:00:00"
     } else {
@@ -14,16 +11,35 @@ function twoHourProgramDisplayed() {
 }
 
 const buttonTwoHourProgram = document.getElementById("2-hour-program")
-buttonTwoHourProgram.addEventListener("click", (event) => {
-    event.preventDefault()
+buttonTwoHourProgram.addEventListener("click", () => {
     twoHourProgramDisplayed()
 })
 
-function twoHourProgram() {
-    // TIMER
-    function timerDisplayed() { }
-    // COUNTDOWN
-    function countdownDisplayed() { }
-    // PROGRESS BAR
-    const progressBar = document.querySelector(".progress-bar")
+function twoHourProgramTimer() { }
+
+function twoHourProgramCountdown() {
+    const globalTime = 2 * 60 * 60
+    let time = globalTime
+    const countdownElement = document.querySelector(".countdown")
+    const interval = setInterval(() => {
+        let hours = parseInt(time / 3600, 10)
+        let minutes = parseInt((time % 3600) / 60, 10)
+        let seconds = time % 60
+        hours = hours < 10 ? "0" + hours : hours
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        seconds = seconds < 10 ? "0" + seconds : seconds
+        countdownElement.innerText = `${hours}:${minutes}:${seconds}`
+        time = time <= 0 ? 0 : time - 1
+        if (time === 0) {
+            clearInterval(interval)
+        }
+    }, 1000)
 }
+
+const startButton = document.querySelector(".start-button")
+startButton.addEventListener("click", () => {
+    const countdownDisplayed = document.querySelector(".countdown")
+    if (countdownDisplayed.innerText === "02:00:00") {
+        twoHourProgramCountdown()
+    }
+})
